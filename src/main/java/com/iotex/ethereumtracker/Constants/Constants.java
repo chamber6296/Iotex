c678e9ce0d94387c83f422ecc6665257d4019527
chamber <chamber@chamberdeMacBook-Pro.local>
2022-02-25 16:18:13 +0800

    change access token

--- src/main/java/com/iotex/ethereumtracker/Constants/Constants.java
+++ src/main/java/com/iotex/ethereumtracker/Constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String LARKROBOTRUL = "https://open.larksuite.com/open-apis/bot/v2/hook/238326aa-4dcf-488a-add8-50e2656e2e0d";
     public static final String SECRET = "Z9ttvx89sgwKCwJ74coKKf";
     public static final String ETHEREUMURL = "https://github.com/ethereum/go-ethereum/";
-    public static final String HITHUBTOKEN = "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";
+    public static final String HITHUBTOKEN = "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";
     public static final String COMMITBASERUL = "https://api.github.com/repos/ethereum/go-ethereum/commits?sha=master&since=2022-02-20T00:00:00&path=";
     public static final String EIPSURL = "https://eips.ethereum.org/all";
     public static final String EIPSSTATUSURL = "https://eips.ethereum.org";
